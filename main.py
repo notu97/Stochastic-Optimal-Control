7ec634ede78b7aa93aa0d081f3dff5895d2eca35
Shiladitya Biswas <shiladitya97@outlook.com>
2020-05-23 05:38:40 -0700

    Final Commit

--- main.py
+++ main.py
@@ -12,35 +12,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from scipy.stats import multivariate_normal as MVN
 from scipy.interpolate import interp1d
+import seaborn as sns
+import pdb
+import time
 
-#-----------Parameters----------------------
-
-tau=0.1
-# n2=8 # x2 discreet
-
-n_u=10 # Num of controlls workred: 10
-
-v_max=10
-u_max=10
-goal=np.zeros((0,0))
-
-n1=151 # x1 discreet angle worked: 101
-n2=41 # round ((tau*(2*v_max))/((2*np.pi)/n1))+1 # x2 discreet. worked= 31
-k=0.1 # damping ratio
-r=1 # 
-a=1 # g/L
-b=0.1 # k/m
-
-gamma=0.1
-sigma=np.array([[1],[1]])
-dist=0.001
-init_state=np.array([np.pi,0])
-
-# FLAGS to select the type of algorithm. Set the choice to 1 and the other to 0
-VI=0 # Value Iteration (VI)
-PI=1 # Policy Iteration (PI)
-
-#---------------------------------
 
 class EnvAnimate:
     '''
@@ -130,69 +105,58 @@ class InvertedPendulum(EnvAnimate):
         # constants: dt, vmax, umax, n1, n2, nu
         # parameters: a, b, σ, k, r, γ
         
-        self.dt = 0.1
+        self.dt =tau
         self.t = np.arange(0.0, 100, self.dt)
         self.load_random_test_trajectory()
         pass
         
-    # TODO: Implement your own VI and PI algorithms
-    # Feel free to edit/delete these functions
-    # def l_xu(self, x1, x2, u):
-    #     # Stage cost
-    #     return
-    
-    # def f_x_u(self,X,u):
-    #     return np.vstack(( X[1], (a*np.sin(wrapp(X[0]) ))-(b*X[1])+u))
-    # def f_xu(self, x1, x2, u):
-    #     # Motion model
-    #     return
     
     def cost(self,X,u):
-        return (1-np.exp(k*(np.cos(X[0]-1)))+ (0.5*r*u*u) )
-
-    def wrapp(self, angles):
-        n=int(abs(angles/ (np.pi) ))+1
-        angle=(-(n*np.pi - angles) * (angles >= np.pi)) + ((n*np.pi+angles)*(angles <= -np.pi))
-        return angle 
+        return (1-np.exp(k*(np.cos(X[0])-1))+ (0.5*r*u*u) )
+    
+    def wrap(self, angles):
+        n= int(np.abs((angles/np.pi)))
+        if(n%2==0):
+            angles=angles-(n*(np.pi)*np.sign(angles))
+        else:
+            angles= angles-(n+1)*np.pi*(np.sign(angles))
+        return angles
     
     def f_x_u(self,X,u):
-        return np.vstack(( X[1], (a*np.sin(self.wrapp(X[0]) ))-(b*X[1])+u))
+        return np.vstack(( X[1], (a*np.sin(X[0]))-(b*X[1])+u))
     
     def loc1(self,x1,x2,len_x1,len_x2):
         l=(x2*len_x1)+(x1)
         return l
 
-    def Build_MDP(self, x1,x2,U):
+    def Build_MDP(self, x1,x2,U,cov1,state_space):
         print('Building the MDP Model')
+        
         len_x1=len(x1)
         len_x2=len(x2)
         nx=len_x1*len_x2 # Number of States
         nu=len(U) # Number of actions
-        P=np.zeros((nx,nx,nu), dtype=np.float16)
+        P=np.zeros((nx,nx,nu),dtype=np.float16)
         L=np.zeros((nx,nu))
 
         # Build MDP
         e=0
-        for i,x_2 in enumerate(x2):
-            for j,x_1 in enumerate(x1):
-                # print('State',[x_1,x_2])
-                for k,u in enumerate(U):
-                    x_next= np.array([[x_1],[x_2]])+(self.f_x_u([x_1,x_2],u)*tau)           
-                    gauss=MVN(x_next.T[0],cov) # add noise
-                    vals=gauss.pdf(states)
-                    ind=np.where(vals>0.001)
-                    temp=np.zeros(np.shape(vals))
+        for theta in (x1):
+            for vel in (x2):
+                for k,action in enumerate(U):
+                    curr_state=np.array([theta,vel])
+                    
+                    x_next= curr_state.reshape(2,1) +(self.f_x_u(curr_state,action)*tau)
+                    if(x_next[1]>v_max):
+                        x_next[1]=v_max
+                    if(x_next[1]<-v_max):
+                        x_next[1]=-v_max
+                    x_next[0]=self.wrap(x_next[0])
 
-                    temp[ind[0],ind[1]]=((vals[ind[0],ind[1]])/(np.sum(vals[ind[0],ind[1]])))            
-                    P[e,:,k]=temp.flatten() # Transition matrix
-                    L[ int(self.loc1(j,i,len_x1,len_x2)),k]=self.cost([x_1,x_2],u) 
+                    P[e,:,k]=MVN.pdf(state_space,mean=x_next.T[0],cov=cov1)
+                    P[e,:,k]=P[e,:,k]/np.sum(P[e,:,k])
+                    L[ e,k]=(self.cost(curr_state,action)*tau)
                 e=e+1
-                # print('-----------------------------------------------------------------')          
-        goal_ind=np.where( (np.logical_and(( np.round(states[:,:,0])==0),( np.round(states[:,:,1])==0)))==True) 
-        # print(goal_ind) 
-        goal_loc=self.loc1(goal_ind[1][0],goal_ind[0][0],len_x1,len_x2)
-        L[goal_loc]-=1
-
         return P,L
 
     def value_iteration(self,P,L,x1,x2,gamma1):
@@ -201,34 +165,34 @@ class InvertedPendulum(EnvAnimate):
         len_x1=len(x1)
         len_x2=len(x2)
         nx=len_x1*len_x2 # Number of States
-        # nu=len(U) # Number of actions
-
-        # a,b=np.shape(L)
-        # isgoal=int(nx/2) #loc()  #int(sz/2) #(((int(b/2))*b))+int(a/2)
+        
         gamma=gamma1
         v=np.zeros((nx,1))
-        # policy=np.ones((nx,1))*( round((len(U)+1)/2) )
         H=np.zeros((nx,len(U)))
 
         itr=1
         while True:
-            # print(itr)
-            #     print(v)
             v_old=v
             for i in range(len(U)):
-                H[:,i]=(L[:,i]+(gamma*P[:,:,i]@ v).reshape(nx))
-            #     print(H)
+                H[:,i]=(L[:,i]+(gamma* (P[:,:,i]) @ v).reshape(1,nx))
+                # sns.heatmap(P[:,:,i])
+                # plt.plot(H[:,i])
+                # plt.show()
+                #  print(H)
             policy=np.argmin(H,axis=1)
             v=(np.min(H,axis=1)).reshape(nx,1)
-            #     v[isgoal]=0
-            # print(np.max(np.abs(v-v_old)))
-            if(np.max(np.abs(v-v_old))<1e-100):
+            
+            # sns.heatmap(v.reshape(len_x2,len_x1),linewidths=0.5)
+            # plt.show()
+    
+            if(np.sum((v-v_old)**2) <0.000001):
                 break
             itr=itr+1
+        print(itr)
 
-        plt.figure()
-        plt.imshow(v.reshape(len_x2,len_x1))
-        plt.show()
+        # sns.heatmap(v.reshape(len_x1,len_x2).T,linewidths=0.5)
+        # plt.show()
+
         V_x, VI_policy = v, policy
         return V_x, VI_policy,
     
@@ -249,10 +213,8 @@ class InvertedPendulum(EnvAnimate):
         itr=1
         while True:
             v_old=v
-            # print(itr)
+        
             for i in np.arange(nx):
-        #         print('itr: ',i)
-        #         print(int(policy[i]))
                 PP[i,:]=P[i,:,int(policy[i]) ]
                 LL[i]=L[i,int(policy[i])]
 
@@ -263,111 +225,151 @@ class InvertedPendulum(EnvAnimate):
 
             policy=np.argmin(H,axis=1)
             v=(np.min(H,axis=1)).reshape(nx,1)
-            # print(np.max(np.abs(v-v_old)))
-            
-            if(np.max(np.abs(v-v_old))<1e-100):
+            if(np.sum((v-v_old)**2) <0.00001):
                 break
                 
             itr=itr+1
-        plt.figure()
-        plt.imshow(v.reshape(len_x2,len_x1))
-        plt.show()
+        print(itr)
+        # sns.heatmap(v.reshape(len_x1,len_x2).T,linewidths=0.5)
+        # plt.show()
         V_x, PI_policy = v, policy
         return V_x, PI_policy,
     
-    def generate_trajectory(self, in_loc, init_state, policy, t):
+    def generate_trajectory(self, in_loc, init_state, policy, t,states,tau,P,n_states):
         print('Generating Trajectory')
+        loc=in_loc
+        angle=[]
+        ctrl=[]
+        while(t>0):  
+            t-=tau
+            theta =states[loc,0]
+            angle.append(theta)
 
-        u=policy[in_loc]
-        ctrl=np.array([u])
+            u1= policy[loc]
+            ctrl.append(u1)
+    
+            loc=np.random.choice(range(n_states),1,p=(P[loc,:,u1]))[0]
 
-        X= np.array([[np.pi],[0]])+ (self.f_x_u(init_state,u)*tau)
-        # X[0]=self.wrapp(X[0])
-        angle=np.array([X[0]])
-
-        # X1=np.array([np.pi,0])
-        c=0
-        while c<100: #(X[0]!=0 ):
-        #     print(X)
-            E=((states[:,:,0]-X[0])**2+(states[:,:,1]-X[1])**2)
-            temp=np.argmin(E)
-            u1= policy[temp]
-            ctrl=np.append(ctrl,u1)
-            
-            ind=np.unravel_index(np.argmin(E, axis=None), E.shape)    
-            X= states[ind[0],ind[1],:].reshape(2,1)+(self.f_x_u(states[ind[0],ind[1],:],u1)*tau)
-            X[0]=self.wrapp(X[0])
-            angle=np.append(angle,X[0])
-            # print(X)
-            c=c+1
-
-        f_u = interp1d(np.arange(len(ctrl)), ctrl,kind='cubic')
-        f_theta = interp1d(np.arange(len(angle)), angle, kind='cubic')
-        # U[policy]
-        w=np.linspace(1,10,1000)
-        # plt.plot(w,f_u(w))
-        # plt.grid()
-        # plt.figure()
-        # plt.plot(w,f_theta(w))
-        # plt.grid()
-        # plt.show()
-        # plt.plot(range(len(policy)),U[policy])
-
-
-        theta, u = (f_theta(w)), (f_u(w))
+            # interp1d(np.arange(0,time,tau),)
+        # theta, u = f_theta(w), f_u(w)
+        theta,u= angle,ctrl
         return theta, u
     
 
 if __name__ == '__main__':
+
+    #-----------Parameters----------------------
+
+    tau=0.1
+    # n2=8 # x2 discreet
+    v_max= 3  #3 
+    u_max= 3
+    # goal=np.zeros((0,0))
+
+    n_u=30 # Num of controlls workred: 10
+    n1= 30  #30  #101  # 101# 151 #101 # x1 discreet angle worked: 101, 151
+    n2= 40  #30#101 # 31#41   #round ((tau*(2*v_max))/((2*np.pi)/n1))+1 # x2 discreet. worked= 31,41
+    k=4 # damping ratio, 0.1
+    r=0.01 #, 1
+    a=1 # g/L, 1
+    b=0.01 # k/m, 0.1
+
+    
+
+    gamma=0.9
+    cov=np.diag([0.001,0.001])
+    # sigma=np.array([[0.1],[0.1]])
+    dist=0.0
+    init_state=np.array([np.pi,0])
+    goal=[0,0]
+
+    # FLAGS to select the type of algorithm. Set the choice to 1 and the other to 0
+    # VI=0 # Value Iteration (VI)
+    # PI=0 # Policy Iteration (PI)
+
+    #---------------------------------
+
     inv_pendulum = InvertedPendulum()
     # print(tau)
-    x1=np.linspace(-np.pi,np.pi,n1)
-    x2=np.linspace(-v_max,v_max,n2)
+    x1=np.linspace(-np.pi,np.pi,n1) # Theta
+    x2=np.linspace(-v_max,v_max,n2) # velocity
     U=np.linspace(-u_max,u_max,n_u)
-    # v=np.zeros((n1,n2))
-    cov= (sigma @ sigma.T)*tau+dist*np.eye(2)
 
+
+    
     len_x1=len(x1)
     len_x2=len(x2)
 
-    X1,X2=np.meshgrid(x1,x2) # x axis --> x2, y axis --> x1
-    states = np.dstack((X1, X2))
-
-    init_ind=np.where( (np.logical_and((states[:,:,0]==init_state[0]),(states[:,:,1]==init_state[1])))==True)
-    in_loc=inv_pendulum.loc1(init_ind[1][0],init_ind[0][0],len_x1,len_x2)
-
-
-    P,L=inv_pendulum.Build_MDP(x1,x2,U)
-    if(VI):
-        VI_V, VI_policy = inv_pendulum.value_iteration(P,L,x1,x2,gamma)
-        theta, u = inv_pendulum.generate_trajectory(in_loc,init_state,VI_policy,0)
-    if(PI):
-        PI_V, PI_policy = inv_pendulum.policy_iteration(P,L,x1,x2,U,gamma)
-        theta, u = inv_pendulum.generate_trajectory(in_loc,init_state,PI_policy,0)
+    state_space=np.zeros(( (n1*n2),2))
     
-    # print(type(theta),type(u))
-    # print(theta)
-    # print(len(u))
-    inv_pendulum.load_trajectory(theta, u)
+    e=0
+    for theta in x1:
+        for vel in x2:
+            state_space[e,0]=theta
+            state_space[e,1]=vel
+            e=e+1
+
+    E=np.sum((state_space-init_state)**2,axis=1) 
+    in_loc=np.argmin(E)
+    
+    init_state=state_space[in_loc,:]
+
+    P,L=inv_pendulum.Build_MDP(x1,x2,U,cov,state_space)
+    
+    VI_V, VI_policy = inv_pendulum.value_iteration(P,L,x1,x2,gamma)
+    theta_vi, u_vi = inv_pendulum.generate_trajectory(in_loc,init_state,VI_policy,10,state_space,tau,P,(n1*n2))
+    inv_pendulum.load_trajectory(theta_vi, u_vi)
     inv_pendulum.start()
 
-    # inv_pendulum.start()
-    ######## Using example functions to get the animation ########
-    # inv_pendulum = InvertedPendulum(*params)
-    # PI_V, PI_policy = inv_pendulum.policy_iteration()
-    # theta, u = inv_pendulum.generate_trajectory()
-    # theta=0
+    PI_V, PI_policy = inv_pendulum.policy_iteration(P,L,x1,x2,U,gamma)
+    theta_pi, u_pi = inv_pendulum.generate_trajectory(in_loc,init_state,PI_policy,10,state_space,tau,P,(n1*n2))
+    inv_pendulum.load_trajectory(theta_pi, u_pi)
+    inv_pendulum.start()
 
-    # u=np.load('control1.npy')
-    
-    # theta=np.load('angle1.npy')
+    plt.figure()
+    plt.subplot(121)
+    sns.heatmap(VI_V.reshape(len_x1,len_x2).T)
+    plt.xlabel('Angle')
+    plt.ylabel('Angular Velocity')
+    plt.title('Value Iteration')
+    plt.subplot(122)
+    sns.heatmap(PI_V.reshape(len_x1,len_x2).T)
+    plt.xlabel('Angle')
+    plt.ylabel('Angular Velocity')
+    plt.title('Policy Iteration')
+    plt.suptitle('tau: ' + str(tau)+ ' v_max: ' + str(v_max)+' u_max: '+ str(u_max)+' n_u: '+str(n_u)+' n_theta: '+str(n1)+' n_vel: '+str(n2)+' k: '+ str(k)+ ' r: '+str(r)+' a: '+str(a)+' b: '+str(b)+' Gamma: '+str(gamma)+'\n Cov:'+str(cov))
+    plt.show()
 
-    # inv_pendulum.load_trajectory(theta, u)
-    # inv_pendulum.start()
-    
-    ######## TODO: Implement functions for visualization ########
-    #############################################################
-    
+    plt.subplot(221)
+    plt.plot(u_vi)
+    plt.grid()
+    plt.xlabel('Time')
+    plt.ylabel('Control input')
+    plt.title('Value Iteration')
+    plt.subplot(223)
+    plt.plot(theta_vi)
+    plt.grid()
+    plt.xlabel('Time')
+    plt.ylabel('Angle')
+    # plt.title('Value Iteration')
+
+    plt.subplot(222)
+    plt.plot(u_pi)
+    plt.grid()
+    plt.xlabel('Time')
+    plt.ylabel('Control input')
+    plt.title('Policy Iteration')
+    plt.subplot(224)
+    plt.plot(theta_pi)
+    plt.grid()
+    plt.xlabel('Time')
+    plt.ylabel('Angle')
+    # plt.title('Policy Iteration')
+
+    plt.suptitle('Angle and control input plot w.r.t.Time')
+    plt.show()
+
+        
     
 
 
